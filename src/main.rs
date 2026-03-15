07e8787e306312dfeded3bc23d256c38e54c96c2
Luca Mandrelli <luca.mandrelli@icloud.com>
2026-03-15 18:30:20 +0100

    feat: activity

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,16 @@ async fn main() {
         .setup(move |ctx, _ready, framework| {
             Box::pin(async move {
                 poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+
+                let is_prod = std::env::var("PROD").unwrap_or_default() != "0";
+                let activity_name = if is_prod {
+                    "you should tag me".to_string()
+                } else {
+                    format!("running v{}", env!("CARGO_PKG_VERSION"))
+                };
+
+                ctx.set_activity(Some(serenity::ActivityData::custom(activity_name)));
+
                 Ok(Data)
             })
         })
